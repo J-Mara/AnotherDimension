08b5512cd0d6e1d9187ffbf1a03780965190b9f1
J-Mara <jmaragakis20@stuy.edu>
2020-10-26 18:05:50 -0400

    has sumRows(), but it returns a hex index for some reason

--- ArrayOps.java
+++ ArrayOps.java
@@ -18,9 +18,25 @@ public class ArrayOps {
     return largest;
   }
 
+  public static int[] sumRows(int[][] matrix){
+    int[] result;
+    result = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      result[i] = sum(matrix[i]);
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
-    int[] trial = {1,2,3,4,5,6,7,8,9,10};
-    System.out.println(sum(trial));
-    System.out.println(largest(trial));
+    int[] arr1 = {};
+    int[] arr2 = {1,2};
+    int[] arr3 = {1,2,3};
+    int[] arr4 = {1,2,3,4};
+    int[] arr5 = {1,2,3,4,5};
+    int[][] a = {arr3, arr5};
+    int[][] b = {arr1, arr2, arr3, arr4, arr5};
+    System.out.println(sum(arr5));
+    System.out.println(largest(arr5));
+    System.out.println(sumRows(b));
   }
 }
