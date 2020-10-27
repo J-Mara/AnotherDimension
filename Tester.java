c62c3cae460e11dc8834aec75dd1b8cea560d079
J-Mara <jmaragakis20@stuy.edu>
2020-10-26 22:41:34 -0400

    Tester code

--- Tester.java
+++ Tester.java
@@ -1,6 +1,21 @@
 public class Tester {
   public static void main(String[] args) {
-    int[] trial = {1,2,3,4,5,6,7,8,9,10};
-    System.out.println(ArrayOps.sum(trial));
+    int[] arr1 = {};
+    int[] arr2 = {1,2};
+    int[] arr3 = {1,2,3};
+    int[] arr4 = {1,2,3,4};
+    int[] arr5 = {1,2,3,4,5};
+    int[][] a = {arr3, arr5};
+    int[][] b = {arr1, arr2, arr3, arr4, arr5};
+    int[][] c = {arr4, arr4, arr4};
+    System.out.println(ArrayOps.sum(arr5));
+    System.out.println(ArrayOps.largest(arr5));
+    System.out.println(ArrayOps.arrToString(ArrayOps.sumRows(b)));
+    System.out.println(ArrayOps.arrToString(ArrayOps.largestInRows(a)));
+    System.out.println(ArrayOps.arrToString(ArrayOps.sumCols(c)));
+    System.out.println(ArrayOps.isRowMagic(b));
+    System.out.println(ArrayOps.isRowMagic(c));
+    System.out.println(ArrayOps.isColMagic(c));
+    System.out.println(ArrayOps.isLocationMagic(c, 2, 3));
   }
 }
